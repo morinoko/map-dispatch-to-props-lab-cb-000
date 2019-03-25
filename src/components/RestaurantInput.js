b54cf6357e2cf87d46d95e003d2216bda69092ee
Felice Forby <feliceforby@gmail.com>
2019-03-25 23:14:18 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -9,7 +9,7 @@ export class RestaurantInput extends Component {
     location: ''
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     });
